276a0fc42d28ce1825dd3e6bfc21af3079e73235
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-08-20 19:28:37 -0400

    fonts: fix font path

--- gui_main.cpp
+++ gui_main.cpp
@@ -1,21 +1,29 @@
 #include "mainwindow.hpp"
 
 #include <QApplication>
+#include <QFile>
 #include <QFontDatabase>
 
 int main(int argc, char* argv[])
 {
+  // Debug log
+  qputenv("QT_ASSUME_STDERR_HAS_CONSOLE", "1");
+
   // Make sure numbers are parsed with dots
   qputenv("LC_ALL", "C.UTF-8");
   QLocale::setDefault(QLocale::C);
   setlocale(LC_ALL, "C.UTF-8");
 
   QApplication app(argc, argv);
+  Q_INIT_RESOURCE(fonts);
 
-  int main_font_id = QFontDatabase::addApplicationFont(":/fonts/DMSans-VariableFont_opsz,wght.ttf");
+  int main_font_id
+      = QFontDatabase::addApplicationFont(":/fonts/DMSans-VariableFont_opsz,wght.ttf");
   int ital_font_id = QFontDatabase::addApplicationFont(":/fonts/DMSans-Italic-VariableFont_opsz,wght.ttf");
+
   QFont font("DM Sans");
   font.setWeight(QFont::Medium);
+  font.setHintingPreference(QFont::HintingPreference::PreferVerticalHinting);
   QApplication::setFont(font);
   QApplication::setApplicationName("Spatial Format Converter");
   QApplication::setOrganizationName("Société des Arts Technologiques");
